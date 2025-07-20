bffb7e3c79719261a7ac69459296d620d337ebbd
hajjoha <hajjoha55@gmail.com>
2025-07-20 19:14:06 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { Header } from './components/Header.tsx';
+import { Header } from './components/Header';
 import { InputPanel } from './components/InputPanel';
 import { FlashcardGrid } from './components/FlashcardGrid';
 import { FlashcardModal } from './components/FlashcardModal';
